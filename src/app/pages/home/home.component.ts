a27fd175891ecfe7b2cb5b8500ab981ce4629148
Kenzo Lierley <kenzo@MacBook-Air-de-Kenzo.local>
2024-12-06 03:12:21 +0100

    end horrible input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ChampHorribleComponent } from '../../components/champ-horrible/champ-horrible.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ChampHorribleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
