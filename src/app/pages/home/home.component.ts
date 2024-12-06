7cf23e4ff3d62fbbe98a92794d6204041e7ea54f
RAIMONDO AXEL p2201074 <118347630+Axelrmd@users.noreply.github.com>
2024-12-06 02:25:52 +0100

    logo random

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogoDeZinzinComponent } from '../../components/logo-de-zinzin/logo-de-zinzin.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LogoDeZinzinComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
